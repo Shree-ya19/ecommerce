ff271d12b075af67b4502292d879dba59bb36214
shree19 <Shreeyakarki19@gmail.com>
2024-11-17 13:45:57 +0545

    first commit

---
+++ Store/urls.py
@@ -0,0 +1,11 @@
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.template.context_processors import static
+from django.urls import path
+from.import views
+
+urlpatterns = [
+    path('',views.store,name="store"),
+    path('cart/',views.cart,name="cart"),
+    path('checkout/',views.checkout,name="checkout"),
+]
+urlpatterns += staticfiles_urlpatterns()#point to remember for css
